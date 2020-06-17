d5703ba91c365fa2af3e79300c5a3044003f3902
Bárbara Xavier <bxstars4@gmail.com>
2020-06-16 23:07:34 -0400

    Adicionada validação para entrada de valores nulo, ao abrir o app.

--- FindCEP/MainPage.xaml.cs
+++ FindCEP/MainPage.xaml.cs
@@ -8,6 +8,8 @@ using Xamarin.Forms;
 using FindCEP.Serviços.Modelo;
 using FindCEP.Serviços;
 using Xamarin.Essentials;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FindCEP
 {
@@ -20,60 +22,59 @@ namespace FindCEP
             BOTAO.Clicked += BuscarCEP;
 
         }
-        private void BuscarCEP(object sender, EventArgs args)
+
+    private void BuscarCEP(object sender, EventArgs args)
         {
 
-            if (Xamarin.Essentials.Connectivity.NetworkAccess != NetworkAccess.Internet)
-            {
-               
-                DisplayAlert("No Internet", "", "Ok"); 
+            StatusConexao();
 
-                return;
+            string cep = string.Empty;
+
+            if (CEP.Text != null)
+            {
+                cep = CEP.Text.Trim();  
             }
-            string cep = CEP.Text.Trim();  //Em UWP validar valores nulos
 
-            if (isValidCEP(cep))    
+            if (isValidCEP(cep))
             {
-               try
+                try
                 {
+                 
                     Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
 
                     if (end != null)
                     {
-                    RESULTADO.Text = string.Format("Endereço : {0}\r\nBairro : {1}\r\nCompl. : {2}\r\nCidade : {3} - {4}", end.logradouro, end.bairro, end.complemento ,end.localidade, end.uf );
+                        RESULTADO.Text = string.Format("Endereço : {0}\r\nBairro : {1}\r\nCompl. : ({2}\r\nCidade : {3} - {4}", end.logradouro, end.bairro, end.complemento, end.localidade, end.uf);
                     }
 
-                     else
+                    else
                     {
                         DisplayAlert("Erro: ", "O endereço não foi encontrado para o CEP informado " + cep, "Ok");
                     }
 
                 }
 
-               catch (Exception e)
+                catch (Exception e)
                 {
-              
                     DisplayAlert("Erro Crítico: ", e.Message, "Ok");
                 }
-
             }
-
         }
 
-        private bool isValidCEP(string cep)
+        public bool isValidCEP(string cep)
         {
             bool valido = true;
 
             if (cep == "")
             {
-                DisplayAlert("Erro: ", "Informe o CEP!", "Ok");
+                DisplayAlert("Informe o CEP!", "", "Ok");
 
                 valido = false;
             }
-            
+
             else if (cep.Length != 8)
             {
-                DisplayAlert("Erro: CEP inválido!", " O CEP deve conter 8 caracteres.", "Ok");
+                DisplayAlert("Alertpa: CEP inválido!", " O CEP deve conter 8 caracteres.", "Ok");
 
                 valido = false;
             }
@@ -82,13 +83,22 @@ namespace FindCEP
 
             if (!int.TryParse(cep, out NovoCEP))
             {
-                DisplayAlert("Erro: CEP inválido!", "O CEP deve ser composto apenas por números", "Ok");
+                DisplayAlert("Alerta: CEP inválido!", "O CEP deve ser composto apenas por números", "Ok");
 
                 valido = false;
             }
 
             return valido;
         }
+
+        public void StatusConexao() //Verifica a conexão com a internet 
+        {
+            if (Xamarin.Essentials.Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                DisplayAlert("No Internet", "", "Ok");
+                return;
+            }
+        }  
 
     }
 }
